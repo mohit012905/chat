f0879c1be22d2ec823e1ae43d9d0aa2e480f2a42
Mohit Raval <mohitraval804@gmail.com>
2026-06-22 20:03:57 +0530

    Describe your changes

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -18,85 +18,95 @@ class ChatController extends Controller
 
         $receiver = User::findOrFail($id);
 
-        // Mark messages as seen
+        // Mark received messages as seen
         Message::where('sender_id', $id)
             ->where('receiver_id', auth()->id())
             ->update([
                 'is_seen' => true
             ]);
 
+        // Fetch conversation
         $messages = Message::where(function ($q) use ($id) {
-
                 $q->where('sender_id', auth()->id())
                   ->where('receiver_id', $id);
-
             })
             ->orWhere(function ($q) use ($id) {
-
                 $q->where('sender_id', $id)
                   ->where('receiver_id', auth()->id());
-
             })
             ->orderBy('id')
             ->get();
 
-        return view(
-            'chat.index',
-            compact('receiver', 'messages')
-        );
+        // Decrypt messages for normal users
+        $messages->transform(function ($message) {
+            try {
+                $message->message = Crypt::decryptString($message->getRawOriginal('message'));
+            } catch (\Exception $e) {
+                $message->message = '[Unable to decrypt message]';
+            }
+
+            return $message;
+        });
+
+        return view('chat.index', compact('receiver', 'messages'));
     }
 
     public function send(Request $request)
     {
-        // Update current user's last seen
         auth()->user()->update([
             'last_seen' => now()
         ]);
 
         $request->validate([
-            'message' => 'required|max:5000000',
+            'message' => 'required|string|max:5000',
             'receiver_id' => 'required|exists:users,id'
         ]);
 
         Message::create([
-    'sender_id'   => auth()->id(),
-    'receiver_id' => $request->receiver_id,
-    'message'     => Crypt::encryptString($request->message),
-    'is_seen'     => false,
-]);
+            'sender_id'   => auth()->id(),
+            'receiver_id' => $request->receiver_id,
+            'message'     => Crypt::encryptString($request->message),
+            'is_seen'     => false,
+        ]);
 
-        return redirect()->route(
-            'chat.index',
-            $request->receiver_id
-        );
+        return redirect()->route('chat.index', $request->receiver_id);
     }
+
     public function typing(Request $request)
-{
-    auth()->user()->update([
-        'typing_to' => $request->receiver_id
-    ]);
+    {
+        auth()->user()->update([
+            'typing_to' => $request->receiver_id
+        ]);
 
-    return response()->json([
-        'success' => true
-    ]);
-}
-public function fetchMessages($id)
-{
-    $messages = Message::where(function ($q) use ($id) {
+        return response()->json([
+            'success' => true
+        ]);
+    }
 
-            $q->where('sender_id', auth()->id())
-              ->where('receiver_id', $id);
+    public function fetchMessages($id)
+    {
+        $messages = Message::where(function ($q) use ($id) {
+                $q->where('sender_id', auth()->id())
+                  ->where('receiver_id', $id);
+            })
+            ->orWhere(function ($q) use ($id) {
+                $q->where('sender_id', $id)
+                  ->where('receiver_id', auth()->id());
+            })
+            ->orderBy('id')
+            ->get();
 
-        })
-        ->orWhere(function ($q) use ($id) {
+        // Decrypt messages for AJAX refresh
+        $messages->transform(function ($message) {
+            try {
+                $message->message = Crypt::decryptString($message->getRawOriginal('message'));
+            } catch (\Exception $e) {
+                $message->message = '[Unable to decrypt message]';
+            }
 
-            $q->where('sender_id', $id)
-              ->where('receiver_id', auth()->id());
+            return $message;
+        });
 
-        })
-        ->orderBy('id')
-        ->get();
-
-    return view('chat.partials.messages', compact('messages'));
-}
+        return view('chat.partials.messages', compact('messages'));
+    }
 }
